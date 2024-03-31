92fd7974ab63eae30dec840f03ef945217e042a7
gitkalaiarasu <codewithkalaiarasu.com>
2024-03-31 21:51:38 +0530

    update_issue_deploy

--- src/app/components/Actions/store/store.tsx
+++
@@ -1,9 +0,0 @@
-import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import authReducer from "../reducers/todos";
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
-});
-
