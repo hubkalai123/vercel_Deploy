a0bee5dfce7266135507bee05bafcd29ff2cbd00
gitkalaiarasu <codewithkalaiarasu.com>
2024-03-31 22:00:22 +0530

    update_issue_deployS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "./components/Actions/store/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
